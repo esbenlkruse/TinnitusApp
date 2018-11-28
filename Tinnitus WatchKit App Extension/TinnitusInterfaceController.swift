79ac2dd2c553f3eef51856efeac3856f9658bb51
esbenlkruse <esbenkruse@gmail.com>
2018-11-28 14:04:59 +0100

    Added slider for level of tinnitus on the Apple Watch, and ability to save it to firebase database.

--- Tinnitus WatchKit App Extension/TinnitusInterfaceController.swift
+++ Tinnitus WatchKit App Extension/TinnitusInterfaceController.swift
@@ -11,14 +11,70 @@ import Foundation
 import Alamofire
 
 
-class TinnitusInterfaceController: WKInterfaceController {
+class TinnitusInterfaceController: WKInterfaceController, CLLocationManagerDelegate {
+    
+    
+    // MARK: Properties
+    
+    /// Location manager to request authorization and location updates.
+    let manager = CLLocationManager()
+    
+    /// Flag indicating whether the manager is requesting the user's location.
+    var isRequestingLocation = false
+    
+    var tinnitusLevel = 2
+    @IBOutlet var saveObservationButton: WKInterfaceButton!
+    
+    /// Label to display an error if the location manager finishes with an error.
+    @IBOutlet var errorLabel: WKInterfaceLabel!
+    
+    
+    // MARK: Localized String Convenience
+    
+    var interfaceTitle: String {
+        return NSLocalizedString("Request", comment: "Indicates that this interface exemplifies requesting location from the watch")
+    }
+    
+    var requestLocationTitle: String {
+        return NSLocalizedString("Request Location", comment: "Button title to indicate that pressing the button will cause the location manager to request location")
+    }
+    
+    var cancelTitle: String {
+        return NSLocalizedString("Cancel", comment: "Cancel the current action")
+    }
+    
+    var savingTitle: String {
+        return NSLocalizedString("Saving...", comment: "Saving the current action")
+    }
+    
+    var deniedText: String {
+        return NSLocalizedString("Location authorization denied.", comment: "Text to indicate authorization status is .Denied")
+    }
+    
+    var unexpectedText: String {
+        return NSLocalizedString("Unexpected authorization status.", comment: "Text to indicate authorization status is an unexpected value")
+    }
+    
+    var latitudeResetText: String {
+        return NSLocalizedString("<latitude reset>", comment: "String indicating that no latitude is shown to the user due to a timer reset")
+    }
+    
+    var longitudeResetText: String {
+        return NSLocalizedString("<longitude reset>", comment: "String indicating that no longitude is shown to the user due to a timer reset")
+    }
+    
+    var errorResetText: String {
+        return NSLocalizedString("<no error>", comment: "String indicating that no error is shown to the user")
+    }
     
     // MARK: Interface Controller
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
+        print(WKInterfaceDevice.current().name)
         
-        // Configure interface objects here.
+        // Remember to set the location manager's delegate.
+        manager.delegate = self
     }
     
     override func willActivate() {
@@ -30,18 +86,59 @@ class TinnitusInterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-    /*
-    func saveToFirebase() {
-        let firebaseUrl = "https://tinnitus-dfcd4.firebaseio.com/test-observations.json"
+    
+    
+    // MARK: Button Actions
+    
+    @IBAction func saveObservation() {
+        guard !isRequestingLocation else {
+            manager.stopUpdatingLocation()
+            isRequestingLocation = false
+            
+            return
+        }
+        
+        let authorizationStatus = CLLocationManager.authorizationStatus()
+        
+        switch authorizationStatus {
+        case .notDetermined:
+            isRequestingLocation = true
+            saveObservationButton.setTitle(savingTitle)
+            manager.requestWhenInUseAuthorization()
+            
+        case .authorizedWhenInUse:
+            isRequestingLocation = true
+            saveObservationButton.setTitle(savingTitle)
+            manager.requestLocation()
+            
+            
+        case .denied:
+            errorLabel.setText(deniedText)
+            
+        default:
+            errorLabel.setText(unexpectedText)
+        }
+    }
+    
+    func saveToFirebase(lat: String, lon: String) {
+        var firebaseUrl = "https://tinnitus-dfcd4.firebaseio.com/observations.json"
+        
+        if (IsDebug) {
+            firebaseUrl = "https://tinnitus-dfcd4.firebaseio.com/test-observations.json"
+        }
         
         let now = Date()
         let formatter = DateFormatter()
         formatter.timeZone = TimeZone.current
         formatter.dateFormat = "dd-MM-yyyy HH:mm:ss"
         let dateString = formatter.string(from: now)
-        
+        let deviceName = WKInterfaceDevice.current().name
         let parameters: Parameters = [
-            "timestamp": "\(dateString)"
+            "timestamp": "\(dateString)",
+            "latitude": lat,
+            "longitude": lon,
+            "deviceName": deviceName,
+            "level": "\(tinnitusLevel)"
         ]
         
         // Post data to firebase database
@@ -58,7 +155,61 @@ class TinnitusInterfaceController: WKInterfaceController {
         }
     }
     
-    @IBAction func saveObservation() {
-        saveToFirebase()
-    } */
+    @IBAction func tinnitusLevelChanged(_ value: Float) {
+        tinnitusLevel = Int(round(value))
+    }
+    
+    
+    // MARK: CLLocationManagerDelegate Methods
+    
+    /**
+     When the location manager receives new locations, display the latitude and
+     longitude of the latest location and restart the timers.
+     */
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else { return }
+        
+        DispatchQueue.main.async {
+            let lastLocationCoordinate = locations.last!.coordinate
+            
+            print(String(lastLocationCoordinate.latitude))
+            
+            self.saveToFirebase(lat: String(lastLocationCoordinate.latitude), lon: String(lastLocationCoordinate.longitude))
+        
+        }
+    }
+    
+    /**
+     When the location manager receives an error, display the error and restart the timers.
+     */
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        DispatchQueue.main.async {
+            self.errorLabel.setText(String(error.localizedDescription))
+            
+            self.isRequestingLocation = false
+
+        }
+    }
+    
+    /**
+     Only request location if the authorization status changed to an authorization
+     level that permits requesting location.
+     */
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        DispatchQueue.main.async {
+            guard self.isRequestingLocation else { return }
+            
+            switch status {
+            case .authorizedWhenInUse:
+                manager.requestLocation()
+                
+            case .denied:
+                self.errorLabel.setText(self.deniedText)
+                self.isRequestingLocation = false
+                
+            default:
+                self.isRequestingLocation = false
+            }
+        }
+    }
 }
